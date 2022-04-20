6dd9513685c064c660eb6dfee3ccf5231dbacbe4
kefrn Bao <keprhen88@gmail.com>
2022-04-20 11:18:55 +0200

    new_version

--- exercice_2.py
+++ exercice_2.py
@@ -4,11 +4,11 @@ def draw_hollow_square(char_to_draw, size):
     for i in range(1, size+1):
         for j in range(1, size+1):
             if i == 1 or i == size or j == 1 or j == size:
-                print(char_to_draw, end="")
+                print(char_to_draw+" ", end="")
             else:
                 print("  ", end="")
 
         print()
 
 
-draw_hollow_square(char_to_draw="0 ", size=5)
+draw_hollow_square(char_to_draw="m", size=7)
