4946e79da7fa8adafad4a9d1934c74d68210c067
kefrn Bao <keprhen88@gmail.com>
2022-04-20 11:12:20 +0200

    deuxieme exo

---
+++ exercice_2.py
@@ -0,0 +1,14 @@
+
+
+def draw_hollow_square(char_to_draw, size):
+    for i in range(1, size+1):
+        for j in range(1, size+1):
+            if i == 1 or i == size or j == 1 or j == size:
+                print(char_to_draw, end="")
+            else:
+                print("  ", end="")
+
+        print()
+
+
+draw_hollow_square(char_to_draw="0 ", size=5)
